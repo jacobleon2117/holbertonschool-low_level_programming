1ea92959ee1851b73c4c918efa3d23b3d704486a
Jacob Leon <jacobleon@Jacobs-MacBook-Air.local>
2023-09-20 12:58:55 -0500

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main -Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("\"With proper grammer, but the outcome is a piece of art,");
+	return(0);
+}
