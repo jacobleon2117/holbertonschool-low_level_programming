e4eff20346e7274008f34e5770f1930f9e6fe50f
Jacob Leon <jacobleon@Jacobs-Air.attlocal.net>
2023-09-23 23:31:28 -0500

    comb

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,9 +1,25 @@
 #inculde <stdio.h>
 /**
+* main - Prints numbers withh commas
 *
-*
-*
+* Return: Always (Success)
 */
 int main(void)
 {
-	
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
