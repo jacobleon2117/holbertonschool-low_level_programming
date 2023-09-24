ee0b8693bc8b8965770d08e318c40c8c43839c3f
Jacob Leon <jacobleon@Jacobs-Air.attlocal.net>
2023-09-23 23:04:26 -0500

    alphabets

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabetic
+ * main - Prints the alphabetic in lower and upper case
  *
  * Return: Always (Success)
  */
 int main(void)
 {
 	char c;
-	
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
