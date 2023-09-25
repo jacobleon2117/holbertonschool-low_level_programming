05634691d11db54d5a80bc9908a166e550db0903
Jacob Leon <jacobleon@Jacobs-Air.attlocal.net>
2023-09-25 17:10:23 -0500

    sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * print_sign - prints the sign of a number
-* @n: the number to be checked 
+* @n: the number to be checked
 *
 * Return: 1 for positive num, -1 for negative num or zero for anything else
 */
