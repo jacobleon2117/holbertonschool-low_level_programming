934977c787fd71cfd61d6de71d4e1cfd3ac72f6a
Jacob Leon <jacobleon@Jacobs-Air.attlocal.net>
2023-09-25 17:08:29 -0500

    isalpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-  * _isalpha - checks for the alphabetic character
-  * @c: the character to be checked
-  *
-  * Return: 1 for alphabetic character or 0 for anything else
-  */
+* _isalpha - checks for the alphabetic character
+* @c: the character to be checked
+*
+* Return: 1 for alphabetic character or 0 for anything else
+*/
 int _isalpha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
