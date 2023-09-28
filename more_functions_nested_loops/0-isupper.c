d013ff966284868fd6ddee76caf2c2d049e77ac1
jacobleon2117 <jacobleon2117@gmail.com>
2023-09-28 09:37:29 -0700

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h" 
+/**
+ * main - entry point
+ *
+ * Return: Always 0 
+ */
+int _isupper(int c);
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
+}
