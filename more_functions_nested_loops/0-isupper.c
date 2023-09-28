1574a67f18177600a6ca9059f9c9b702c68dec33
jacobleon2117 <jacobleon2117@gmail.com>
2023-09-28 12:32:09 -0700

    isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,17 @@
 #include "main.h"
 /**
  * main - entry point
+ *@c: char to check
  *
- * Return: Always 0
+ *
+ * Return:  0 or 1
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
+	}
+{
 	else
 
 		return (0);
