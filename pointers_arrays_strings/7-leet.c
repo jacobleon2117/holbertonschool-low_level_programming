5a3916262beaaaaf92f8f8d68c2f8793610ee899
jacobleon2117 <jacobleon2117@gmail.com>
2023-10-08 18:14:26 -0700

    leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
- * leet - encode a string into 1337
- * @s: string
- * Return: encoded string `s`
+ * leet - converts a string into leetspeak
+ * @str: the string destined to become 1337
+ *
+ * Return: th3 n3w1y 13373d 2tr1ng
  */
 
-char *leet(char *s)
+char *leet(char *str)
 {
-	int i;
+	char en[] = {'a', 'e', 'o', 't', 'l'};
+	char EN[] = {'A', 'E', 'O', 'T', 'L'};
+	char le[] = {'4', '3', '0', '7', '1'};
+	int i = 0;
+	int j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (str[i] != '\0')
 	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (j = 0; j < 5; j++)
+		{
+			if (str[i] == en[j] || str[i] == EN[j])
+				str[i] = le[j];
+		}
+		i++;
 	}
-
-	return (s);
+	return (str);
 }
