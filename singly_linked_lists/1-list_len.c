e57671785f48c797459ca64aaefd0f2c06292f2a
jacobleon2117 <jacobleon2117@gmail.com>
2023-11-13 00:17:15 -0600

    fixing whitespace and description

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 #include <stdio.h>
-#include <stdlib.h
+#include <stdlib.h>
 
 /**
- *
- *
- *
-**/
+ * list_len - finds the number of nodes in the singly list
+ * @h: pointer to first node in linked list
+ * Return: number of nodes
+ **/
 
 size_t list_len(const list_t *h)
 {
