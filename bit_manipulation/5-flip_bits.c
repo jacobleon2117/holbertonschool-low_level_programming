8e3b6d1325276afa01c60e885e556192b5863c4a
jacobleon2117 <jacobleon2117@gmail.com>
2023-11-13 16:14:32 -0600

    adding files and code

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * flip_bits - the number of bits that is needed to be flipped to get from x to y
+ * @x: First integer
+ * @y: Second integer
+ * Return: flipped bits
+*/
+
+unsigned int flip_bits(unsigned long int x, unsigned long int y)
+{
+	unsigned long int flipped_bits = x ^ y;
+	unsigned int count = 0;
+
+	while (flipped_bits > 0)
+	{
+		if (flipped_bits & 1)
+			count++;
+		flipped_bits >>= 1;
+	}
+	return (count);
+}
